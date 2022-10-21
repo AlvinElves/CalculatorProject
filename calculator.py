3a260f6cab005adc0d842322a6db44321c691567
AlvinElves <hong3842@gmail.com>
2022-10-21 21:16:42 +0100

    Added multiple expression calculator

--- calculator.py
+++ calculator.py
@@ -15,73 +15,88 @@ def press_number_button(number):
 
 
 def press_function_button(expression):
-    global expression_operator
-    global total_equation_text
+    global type_chose
     global equation_text
-    global number_1
-    global number_2
-    if not expression_operator:
-        if '.' in equation_text:
-            number_1 = float(equation_text)
+    if type_chose == 1:
+        global expression_operator
+        global total_equation_text
+        global number_1
+        global number_2
+        if not expression_operator:
+            if '.' in equation_text:
+                number_1 = float(equation_text)
+            else:
+                number_1 = int(equation_text)
+
+            equation_text = ""
+            expression_operator = expression
+            total_equation_text = str(number_1) + " " + expression
+            total_equation_label.set(total_equation_text)
+
         else:
-            number_1 = int(equation_text)
+            if '.' in equation_text:
+                number_2 = float(equation_text)
+            else:
+                number_2 = int(equation_text)
 
-        equation_text = ""
-        expression_operator = expression
-        total_equation_text = str(number_1) + " " + expression
-        total_equation_label.set(total_equation_text)
+            if expression_operator == "+":
+                number_1 = number_1 + number_2
+            elif expression_operator == "-":
+                number_1 = number_1 - number_2
+            elif expression_operator == "*":
+                number_1 = number_1 * number_2
+            elif expression_operator == "/":
+                number_1 = number_1 / number_2
 
+            equation_text = ""
+            expression_operator = expression
+            total_equation_text = str(number_1) + " " + expression
+            total_equation_label.set(total_equation_text)
+            equation_label.set(str(number_1))
     else:
+        if equation_text == "0":
+            equation_text = str(expression)
+        else:
+            equation_text = equation_text + str(expression)
+        equation_label.set(equation_text)
+
+
+def press_equal_button():
+    global type_chose
+    global equation_text
+    global result
+    global equal_pressed
+    if type_chose == 1:
+        global expression_operator
+        global total_equation_text
+        global number_2
+        equal_pressed = True
         if '.' in equation_text:
             number_2 = float(equation_text)
         else:
             number_2 = int(equation_text)
+        equation_text = ""
+        total_equation_text = total_equation_text + " " + str(number_2) + " ="
+        total_equation_label.set(total_equation_text)
 
         if expression_operator == "+":
-            number_1 = number_1 + number_2
+            result = number_1 + number_2
         elif expression_operator == "-":
-            number_1 = number_1 - number_2
+            result = number_1 - number_2
         elif expression_operator == "*":
-            number_1 = number_1 * number_2
+            result = number_1 * number_2
         elif expression_operator == "/":
-            number_1 = number_1 / number_2
+            result = number_1 / number_2
 
-        equation_text = ""
-        expression_operator = expression
-        total_equation_text = str(number_1) + " " + expression
-        total_equation_label.set(total_equation_text)
-        equation_label.set(str(number_1))
+        equation_label.set(str(result))
 
-
-def press_equal_button():
-    global expression_operator
-    global total_equation_text
-    global equation_text
-    global number_2
-    global result
-    global equal_pressed
-    equal_pressed = True
-    if '.' in equation_text:
-        number_2 = float(equation_text)
+        total_equation_text = ""
+        expression_operator = ""
     else:
-        number_2 = int(equation_text)
-    equation_text = ""
-    total_equation_text = total_equation_text + " " + str(number_2) + " ="
-    total_equation_label.set(total_equation_text)
-
-    if expression_operator == "+":
-        result = number_1 + number_2
-    elif expression_operator == "-":
-        result = number_1 - number_2
-    elif expression_operator == "*":
-        result = number_1 * number_2
-    elif expression_operator == "/":
-        result = number_1 / number_2
-
-    equation_label.set(str(result))
-
-    total_equation_text = ""
-    expression_operator = ""
+        equal_pressed = True
+        result = eval(equation_text)
+        equation_label.set(str(result))
+        total_equation_label.set(" ".join(equation_text) + " =")
 
 
 def reset():
@@ -95,19 +110,22 @@ def reset():
     expression_operator = ""
     total_equation_text = ""
     equation_text = ""
-    number_1=0
-    number_2=0
+    number_1 = 0
+    number_2 = 0
     equation_label.set(equation_text)
     total_equation_label.set(total_equation_text)
 
 
 def press_negative_button():
-    global equation_text
-    if '.' in equation_text:
-        equation_text = str(-float(equation_text))
-    else:
-        equation_text = str(-int(equation_text))
-    equation_label.set(equation_text)
+    try:
+        global equation_text
+        if '.' in equation_text:
+            equation_text = str(-float(equation_text))
+        else:
+            equation_text = str(-int(equation_text))
+        equation_label.set(equation_text)
+    except:
+        print("Cannot use negative")
 
 
 def press_delete_button():
@@ -116,88 +134,125 @@ def press_delete_button():
     if length == 1:
         equation_text = "0"
     else:
-        equation_text = equation_text[:length-1]
+        equation_text = equation_text[:length - 1]
     equation_label.set(equation_text)
+
+
+def press_type_button():
+    global type_chose
+    if type_chose == 1:
+        type_chose = 2
+        type_label.set("Type: Multiple")
+        type_Single["state"] = "normal"
+        type_Multiple["state"] = "disabled"
+        reset()
+    else:
+        type_chose = 1
+        type_label.set("Type: Single")
+        type_Single["state"] = "disabled"
+        type_Multiple["state"] = "normal"
+        reset()
 
 
 screen = Tk()
 screen.title("Simple Calculator")
-screen.configure(background='black')
-screen.geometry("350x565")
+screen.configure(background='gray')
+screen.geometry("338x650")
 
 equation_text = ""
 total_equation_text = ""
 expression_operator = ""
+type_label_text = "Type: Single"
 number_1 = 0
 number_2 = 0
 result = 0
 equal_pressed = False
 total_equation_label = StringVar()
 equation_label = StringVar()
+type_label = StringVar()
+type_label.set(type_label_text)
+type_chose = 1
 
+type_text_field = Label(screen, textvariable=type_label, bg="white", fg="red", height=3, width=12)
+type_text_field.grid(column=2, row=0, rowspan=2)
 
-total_text_field = Label(screen, textvariable=total_equation_label, font=("Arial", 12), bg="white", fg="gray40", width=38, anchor="e")
-total_text_field.grid(columnspan=5, row=0)
+type_Single = Button(screen, text="Single Expression", fg="black", height=2, width=14, bg="white",
+                     command=lambda: press_type_button())
+type_Single.grid(column=0, row=0, columnspan=2, pady=3)
+type_Single["state"] = "disabled"
 
-text_field = Label(screen, textvariable=equation_label, font=("Arial", 24), bg="white", width=18, height=2, anchor="e")
-text_field.grid(columnspan=5, row=1)
+type_Multiple = Button(screen, text="Multiple Expression", fg="black", height=2, width=14, bg="white",
+                       command=lambda: press_type_button())
+type_Multiple.grid(column=0, row=1, columnspan=2)
+
+total_text_field = Label(screen, textvariable=total_equation_label, font=("Arial", 12), bg="gray", fg="white", width=34,
+                         anchor="e")
+total_text_field.grid(columnspan=5, row=2)
+
+text_field = Label(screen, textvariable=equation_label, font=("Arial", 24), bg="white", width=16, height=2, anchor="e")
+text_field.grid(columnspan=5, row=3)
 
 button_1 = Button(screen, text=1, height=3, width=6, font=10, command=lambda: press_number_button(1))
-button_1.grid(column=0, row=6)
+button_1.grid(column=0, row=8)
 
 button_2 = Button(screen, text=2, height=3, width=6, font=10, command=lambda: press_number_button(2))
-button_2.grid(column=1, row=6)
+button_2.grid(column=1, row=8)
 
 button_3 = Button(screen, text=3, height=3, width=6, font=10, command=lambda: press_number_button(3))
-button_3.grid(column=2, row=6)
+button_3.grid(column=2, row=8)
 
 button_4 = Button(screen, text=4, height=3, width=6, font=10, command=lambda: press_number_button(4))
-button_4.grid(column=0, row=5, pady=5)
+button_4.grid(column=0, row=7, pady=5)
 
 button_5 = Button(screen, text=5, height=3, width=6, font=10, command=lambda: press_number_button(5))
-button_5.grid(column=1, row=5)
+button_5.grid(column=1, row=7)
 
 button_6 = Button(screen, text=6, height=3, width=6, font=10, command=lambda: press_number_button(6))
-button_6.grid(column=2, row=5)
+button_6.grid(column=2, row=7)
 
 button_7 = Button(screen, text=7, height=3, width=6, font=10, command=lambda: press_number_button(7))
-button_7.grid(column=0, row=4)
+button_7.grid(column=0, row=6, padx=7)
 
 button_8 = Button(screen, text=8, height=3, width=6, font=10, command=lambda: press_number_button(8))
-button_8.grid(column=1, row=4)
+button_8.grid(column=1, row=6)
 
 button_9 = Button(screen, text=9, height=3, width=6, font=10, command=lambda: press_number_button(9))
-button_9.grid(column=2, row=4)
+button_9.grid(column=2, row=6, padx=7)
 
 button_0 = Button(screen, text=0, height=3, width=6, font=10, command=lambda: press_number_button(0))
-button_0.grid(column=1, row=7, pady=5)
+button_0.grid(column=1, row=9, pady=5)
 
 button_plus_minus = Button(screen, text="+/-", height=3, width=6, font=10, command=lambda: press_negative_button())
-button_plus_minus.grid(column=0, row=7)
+button_plus_minus.grid(column=0, row=9)
 
 button_dot = Button(screen, text=".", height=3, width=6, font=10, command=lambda: press_number_button("."))
-button_dot.grid(column=2, row=7)
+button_dot.grid(column=2, row=9)
 
-button_equal = Button(screen, text="=", bg="lightblue", height=3, width=6, font=10, command=lambda: press_equal_button())
-button_equal.grid(column=3, row=7)
+button_equal = Button(screen, text="=", bg="lightblue", height=3, width=6, font=10,
+                      command=lambda: press_equal_button())
+button_equal.grid(column=3, row=9)
 
-button_plus = Button(screen, text="+", bg="white", height=3, width=6, font=10, command=lambda: press_function_button("+"))
-button_plus.grid(column=3, row=6)
+button_plus = Button(screen, text="+", bg="white", height=3, width=6, font=10,
+                     command=lambda: press_function_button("+"))
+button_plus.grid(column=3, row=8)
 
-button_minus = Button(screen, text="-", bg="white", height=3, width=6, font=10, command=lambda: press_function_button("-"))
-button_minus.grid(column=3, row=5)
+button_minus = Button(screen, text="-", bg="white", height=3, width=6, font=10,
+                      command=lambda: press_function_button("-"))
+button_minus.grid(column=3, row=7)
 
-button_multiply = Button(screen, text="*", bg="white", height=3, width=6, font=10, command=lambda: press_function_button("*"))
-button_multiply.grid(column=3, row=4)
+button_multiply = Button(screen, text="*", bg="white", height=3, width=6, font=10,
+                         command=lambda: press_function_button("*"))
+button_multiply.grid(column=3, row=6)
 
-button_divide = Button(screen, text="/", bg="white", height=3, width=6, font=10, command=lambda: press_function_button("/"))
-button_divide.grid(column=3, row=3)
+button_divide = Button(screen, text="/", bg="white", height=3, width=6, font=10,
+                       command=lambda: press_function_button("/"))
+button_divide.grid(column=3, row=5)
 
-button_delete = Button(screen, text="BACK", bg="white", height=3, width=6, font=10, command=lambda: press_delete_button())
-button_delete.grid(column=2, row=3, pady=5)
+button_delete = Button(screen, text="BACK", bg="white", height=3, width=6, font=10,
+                       command=lambda: press_delete_button())
+button_delete.grid(column=2, row=5, pady=5)
 
 button_clear = Button(screen, text="CLEAR", bg="red", height=3, width=13, font=10, command=lambda: reset())
-button_clear.grid(column=0, row=3, columnspan=2)
-
+button_clear.grid(column=0, row=5, columnspan=2)
 
 screen.mainloop()
